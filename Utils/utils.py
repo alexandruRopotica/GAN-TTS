a0c1313ff62dba60dc4e1058763868524dc996f0
Bolec <alexandru.ropotica@gmail.com>
2021-03-09 13:53:37 +0100

    Added spectral normalization

--- Utils/utils.py
+++ Utils/utils.py
@@ -15,6 +15,67 @@ class OrthogonalRegularizer(tf.keras.regularizers.Regularizer):
         return outputs
 
 
+class SpectralNormalization(tf.keras.layers.Wrapper):
+    def __init__(self, layer, iteration=1, eps=1e-12, training=True, **kwargs):
+        self.iteration = iteration
+        self.eps = eps
+        self.do_power_iteration = training
+        if not isinstance(layer, tf.keras.layers.Layer):
+            raise ValueError(
+                'Please initialize `TimeDistributed` layer with a '
+                '`Layer` instance. You passed: {input}'.format(input=layer))
+        super(SpectralNormalization, self).__init__(layer, **kwargs)
+
+    def build(self, input_shape):
+        self.layer.build(input_shape)
+
+        self.w = self.layer.kernel
+        self.w_shape = self.w.shape.as_list()
+
+        self.v = self.add_weight(shape=(1, self.w_shape[0] * self.w_shape[1]),
+                                 initializer=tf.initializers.TruncatedNormal(stddev=0.02),
+                                 trainable=False,
+                                 name='sn_v',
+                                 dtype=tf.float32)
+
+        self.u = self.add_weight(shape=(1, self.w_shape[-1]),
+                                 initializer=tf.initializers.TruncatedNormal(stddev=0.02),
+                                 trainable=False,
+                                 name='sn_u',
+                                 dtype=tf.float32)
+
+        super(SpectralNormalization, self).build()
+
+    def call(self, inputs):
+        self.update_weights()
+        output = self.layer(inputs)
+        self.restore_weights()  # Restore weights because of this formula "W = W - alpha * W_SN`"
+        return output
+    
+    def update_weights(self):
+        w_reshaped = tf.reshape(self.w, [-1, self.w_shape[-1]])
+        
+        u_hat = self.u
+        v_hat = self.v  # init v vector
+
+        if self.do_power_iteration:
+            for _ in range(self.iteration):
+                v_ = tf.matmul(u_hat, tf.transpose(w_reshaped))
+                v_hat = v_ / (tf.reduce_sum(v_**2)**0.5 + self.eps)
+
+                u_ = tf.matmul(v_hat, w_reshaped)
+                u_hat = u_ / (tf.reduce_sum(u_**2)**0.5 + self.eps)
+
+        sigma = tf.matmul(tf.matmul(v_hat, w_reshaped), tf.transpose(u_hat))
+        self.u.assign(u_hat)
+        self.v.assign(v_hat)
+
+        self.layer.kernel.assign(self.w / sigma)
+
+    def restore_weights(self):
+        self.layer.kernel.assign(self.w)
+
+
 class SpectralConv1D(tf.keras.layers.Layer):
     def __init__(self, filters, kernelSize, strides=1,
                 padding='same', dilation=1, activation=None,
@@ -29,7 +90,7 @@ class SpectralConv1D(tf.keras.layers.Layer):
         self.activation = activation
         self.kernelInit = kernelInit
         self.kernelReg = kernelReg
-        self.spectralConv = tfa.layers.SpectralNormalization(
+        self.spectralConv = SpectralNormalization(
             tf.keras.layers.Conv1D(
                 filters=self.filters, kernel_size=self.kernelSize, strides=self.strides,
                 padding=self.padding, dilation_rate=self.dilation, activation=self.activation,
@@ -51,7 +112,7 @@ class SpectralConv1DTranspose(tf.keras.layers.Layer):
         self.padding = padding
         self.kernelInit = kernelInit
         self.kernelReg = kernelReg
-        self.spectralConvTranspose = tfa.layers.SpectralNormalization(
+        self.spectralConvTranspose = SpectralNormalization(
             tf.keras.layers.Conv1DTranspose(
                 filters=self.filters, kernel_size=self.kernelSize,
                 strides=self.strides, padding=self.padding,
