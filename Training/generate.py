e13288705be9a33e4f9da91fbc886e14fceef0f4
Bolec <alexandru.ropotica@gmail.com>
2021-03-15 12:33:36 +0100

    Updated generation procecss

--- Training/generate.py
+++ Training/generate.py
@@ -9,23 +9,34 @@ from Models.bert import BERT
 from Models.FeatureNet.cbhg import CBHG
 from Models.GeneratorNet.generator import Generator
 from Models.DiscriminatorNet.discriminator import Discriminator
+import numpy as np
 
 
 DATASET_DIR = './LJspeechTest'
 CKPT = './Checkpoints/'
 
 
+def FrechetInceptionDistance(realAudio, generatedAudio):
+    inceptionv3 = tf.keras.applications.InceptionV3(include_top=False, input_shape=(128,125,3), pooling='avg')
+    realFeatures = inceptionv3(realAudio)
+    generatedFeatures = inceptionv3(generatedAudio)
+    muReal, sigmaReal = np.mean(realFeatures), np.cov(realFeatures, rowvar=False)
+    muGenerated, sigmaGenerated = np.mean(generatedFeatures), np.cov(generatedFeatures, rowvar=False)
+    muDiff = np.linalg.norm(muReal- muGenerated)
+    covDiff = sigmaReal + sigmaGenerated - 2*(tf.math.sqrt(sigmaReal*sigmaGenerated))
+    fid = muDiff**2 + np.trace(covDiff)
+    return fid
+
 
 def saveGeneratedAudio():
-    genOptimizer, discOptimizer = None
-    generator = Generator(10, False)
-    featureNet = CBHG(BATCH_SIZE, 10, True, 768)
-    dicriminator = Discriminator()
     checkpoint = tf.train.Checkpoint(genOptimizer=genOptimizer,
                                  discOptimizer=discOptimizer,
                                  generator=generator,
                                  discriminator=discriminator,
                                  featureNet=featureNet)
-    
-    checkpoint.restore(os.path.join(CKPT, 'ckpt-1.data-00000-of-00001'))
-    print(generator.summary)
+    models = checkpoint.restore(os.path.join(CKPT, 'ckpt-1.data-00000-of-00001'))
+    print(models.generator.summary())
+
+
+if __name__=='__main__':
+    saveGeneratedAudio()
